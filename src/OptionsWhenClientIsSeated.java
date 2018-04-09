cf8cf29187e43135888400411833a39cd6939406
Regwyn <Regwyn@abv.bg>
2018-04-09 15:56:18 +0300

    Reupload Main and Table and added Seat and OptionsWhenClientIsSeated classes

---
+++ src/OptionsWhenClientIsSeated.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class OptionsWhenClientIsSeated extends JFrame {
+
+    private JFrame jFrame;
+    private JButton order;
+    private JButton pay;
+    private JButton leaveWithoutPaying;
+    private JPanel clientSeatNumberAndTableNumber;
+    private JLabel textForThePanel;
+
+    public OptionsWhenClientIsSeated() throws HeadlessException {
+        jFrame = new JFrame();
+        order = new JButton("ORDER");
+        pay = new JButton("PAY");
+        leaveWithoutPaying = new JButton("LEAVE (WITHOUT PAYING)");
+        clientSeatNumberAndTableNumber = new JPanel();
+        textForThePanel = new JLabel("WHAT WOULD THE CLIENT LIKE TO DO ?");
+
+        clientSeatNumberAndTableNumber.setBackground(new java.awt.Color(0, 204, 204));
+        clientSeatNumberAndTableNumber.setBounds(160, 25, 285, 85);
+        clientSeatNumberAndTableNumber.setOpaque(true);
+        clientSeatNumberAndTableNumber.setLayout(new GridBagLayout());
+        clientSeatNumberAndTableNumber.setVisible(true);
+
+        order.setBounds(160,130,285,45);
+        pay.setBounds(160,195,285,45);
+        leaveWithoutPaying.setBounds(160,260,280,45);
+
+
+        jFrame.setSize(600, 425);
+        jFrame.setLayout(new GridBagLayout());
+        jFrame.getContentPane().setLayout(null);
+        jFrame.setResizable(false);
+        jFrame.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        jFrame.setContentPane(new JLabel(new ImageIcon(getClass().getResource("Restaurant2.jpg"))));
+        jFrame.setVisible(true);
+        clientSeatNumberAndTableNumber.add(textForThePanel);
+        jFrame.add(clientSeatNumberAndTableNumber);
+        jFrame.add(order);
+        jFrame.add(pay);
+        jFrame.add(leaveWithoutPaying);
+        validate();
+    }
+}
